843baae2b229022ac1bbd12f9c15fef1586e048e
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-25 11:35:35 -0600

    create test file for index

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Markets Index Page" do
-  
-  
+  it "visit the markets index page" do
+    visit 'api/v0/markets'
+
+    expect(page).to have_content("Markets")
+  end
 end
