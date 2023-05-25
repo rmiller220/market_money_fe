81550f56e206d2d9c1ff90de02798e6055d95222
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-25 11:31:13 -0600

    create market facade

--- app/facades/market_facade.rb
+++ app/facades/market_facade.rb
@@ -1,3 +1,17 @@
 class MarketFacade
-  
+  def get_market_data
+    market_data.map do |data|
+      Market.new(data)
+    end
+  end
+
+  private
+
+  def service
+    @_service ||= MarketService.new
+  end
+
+  def market_data
+    @_market_data ||= service.all_markets[:data]
+  end
 end
